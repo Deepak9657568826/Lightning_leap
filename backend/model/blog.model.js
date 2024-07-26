723d301ce72333a06d1099305fa8e04f8d04d023
Deepak Wayzode <dwayzode@gmail.com>
2024-07-26 11:49:39 +0530

    date added

--- backend/model/blog.model.js
+++ backend/model/blog.model.js
@@ -13,6 +13,10 @@ const blogSchema = mongoose.Schema({
         type: String,
         required: true
     }, 
+    date: {
+        type: Date,
+        default: Date.now 
+    },
     creatorID:{
         type: String,
     }, 
